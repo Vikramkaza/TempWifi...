63ed2fd919855edb1676414fd190154774c72b31
Raj Kisan S <77163828+Rajkisan@users.noreply.github.com>
2022-12-31 02:00:35 +0530

    Update main.ino

--- TempWifi/main/main.ino
+++ TempWifi/main/main.ino
@@ -1,133 +1,163 @@
-#include "EEPROM.h"
-#include "HTML.h"
-#include <WebServer.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include "Fetch.h"
-#define ONE_WIRE_BUS 22        
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
-WebServer server(80);
-
-#include "WiFiManager.h"
-
-
-  unsigned long int timer;
-  long previousMillis = 0;
-  int interval = 10000; 
-  
-
-void setup() {
-     
-  pinMode(2, OUTPUT);
-  pinMode(15,INPUT); //for resetting WiFi creds
-  EEPROM.begin(600);
-  Serial.begin(115200);
-  if(!CheckWIFICreds()){
-    Serial.println("No WIFI credentials stored in memory. Loading form...");
-    digitalWrite(2,HIGH);
-    while(loadWIFICredsForm());
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
-  else{
-    String s = EEPROM.readString(100);
-    String p = EEPROM.readString(200);
-    String d = EEPROM.readString(400); 
-    const char * ssid = s.c_str();
-    const char * password = p.c_str();
-  
-    Serial.println(s+p);
-    WiFi.begin(ssid, password);
-   
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(1000);
-      Serial.println("Connecting to WiFi..");
-    }
-    
-    Serial.println("Connected to the WiFi network");
-     Systemtemp();
-   
-  }
-      
-  
-  
-  
-}
-void wipeEEPROM(){
-  for(int i=0;i<600;i++){
-    EEPROM.writeByte(i,0);
-  }
-  EEPROM.commit();
+  server.send(404, "text/plain", message);
 }
 
-void loop() {
+/*
+ * Function for writing WiFi creds to EEPROM
+ * Returns: true if save successful, false if unsuccessful
+ */
+bool writeToMemory(String ssid, String pass, String device, String minimum, String maximum){
+  char buff1[30];
+  char buff2[30];
+  char buff3[30];
+  char buff4[30];
+  char buff5[30];
+  ssid.toCharArray(buff1,30);
+  pass.toCharArray(buff2,30);
+  device.toCharArray(buff3,30);
+  minimum.toCharArray(buff4,30);
+  maximum.toCharArray(buff5,30);
+  EEPROM.writeString(100,buff1);
+  EEPROM.writeString(200,buff2);
+  EEPROM.writeString(400,buff3);
+  EEPROM.writeString(500,buff4);
+  EEPROM.writeString(600,buff5);
+  delay(100);
+  String s = EEPROM.readString(100);
+  String p = EEPROM.readString(200);
+  String d = EEPROM.readString(400);
+  String mi= EEPROM.readString(500);
+  String ma= EEPROM.readString(600);
+  
+  //#if DEBUG
+  Serial.print("Stored SSID, password,deviceid are: ");
+  Serial.print(s);
+  Serial.print(" / ");
+  Serial.print(p);
+  Serial.print(" / ");
+  Serial.print(d);
+  Serial.print(" / ");
+  Serial.print(mi);
+  Serial.print(" / ");
+  Serial.print(ma);
+  //#endif
+  if(ssid == s && pass == p && device ==d && minimum==mi && maximum==ma){
+    return true;  
+  }else{
+    return false;
+  }
+}
+
+
+/*
+ * Function for handling form
+ */
+void handleSubmit(){
+  String response_success="<h1>Success</h1>";
+  response_success +="<h2>Device will restart in 3 seconds</h2>";
+
+  String response_error="<h1>Error</h1>";
+  response_error +="<h2><a href='/'>Go back</a>to try again";
+  
+  if(writeToMemory(String(server.arg("ssid")),String(server.arg("password")),String(server.arg("deviceid")),String(server.arg("mintemp")),String(server.arg("maxtemp")))){
+     server.send(400, "text/html", response_success);
+     EEPROM.commit();
+     delay(4000);
+     ESP.restart();
+  }else{
+     server.send(400, "text/html", response_error);
+  }
+}
+
+/*
+ * Function for home page
+ */
+void handleRoot() {
+  if (server.hasArg("ssid")&& server.hasArg("password")&& server.hasArg("deviceid")&& server.hasArg("mintemp")&& server.hasArg("maxtemp")) {
+    handleSubmit();
+  }
+  else {
+    server.send(400, "text/html", INDEX_HTML);
+  }
+}
+
+/*
+ * Function for loading form
+ * Returns: false if no WiFi creds in EEPROM
+ */
+bool loadWIFICredsForm(){
+  String s = EEPROM.readString(100);
+  String p = EEPROM.readString(200);
+  String d = EEPROM.readString(400);
+  String mi = EEPROM.readString(500);
+  String ma = EEPROM.readString(600);
+  
+  const char* ssid     = "WIFI-MANAGER-BM";
+  const char* password = "solamaten";
+
+  Serial.println("Setting Access Point...");
+  
+  WiFi.softAP(ssid, password);
+  
+  IPAddress IP = WiFi.softAPIP();
+  
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+  
+  server.on("/", handleRoot);
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  
+  Serial.println("HTTP server started");
  
-  digitalWrite(2,HIGH);
-  delay(1000);
-  digitalWrite(2,LOW);
-  delay(1000);
-  
-}
-void Systemtemp(){
-  if(WiFi.status() == WL_CONNECTED){
-     
-  if(digitalRead(15) == HIGH){
-    Serial.println("Wiping WiFi credentials from memory...");
-    wipeEEPROM();
-    while(loadWIFICredsForm());
+  while(s.length() <= 0 && p.length() <= 0 && d.length() <= 0){
+    server.handleClient();
+    delay(100);
   }
   
-  if (millis() - previousMillis > interval) 
-    {
-    sensors.requestTemperatures();
-    }
-
-    Serial.println("Requesting temperatures: ");
-    Serial.println("Celcius Temperature="+String(sensors.getTempCByIndex(0))+"degC - Fahrenheit Temperature="+String(sensors.getTempFByIndex(0))+"F");
-    
-
-    float x = sensors.getTempCByIndex(0);
-    String str2;
-    str2 = String(x);
-   
-  
-    
-    if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
-      
-      //HTTPClient http;
-      String d = EEPROM.readString(400); 
-      String str1= "https://script.google.com/macros/s/AKfycbxDDwlLMk5pNpAJ98ALu_JxbJqt6cJusyQkcVHUjep78tPgtReJExFkP5fPOhOSB_3K/exec?Temperature=";
-      String str3=str1+str2;
-      String str4= "&DeviceId=";
-      String str5=str4+d;
-      String str6=str3+str5;
-     
-      Serial.println(str6);
-      
-      RequestOptions options;
-      options.method = "GET";
-      const char * str7 = str6.c_str();
-      
-     
-      Response response = fetch(str7, options);
-      Serial.println(str7);
-     
-      Serial.println(response);
-      
-      
-      previousMillis = millis();
-      delay(1000);
-   
-      Systemtemp();
-      
-}
-}
-else if(!CheckWIFICreds()){
-    Serial.println("No WIFI credentials stored in memory. Loading form...");
-    digitalWrite(2,HIGH);
-    while(loadWIFICredsForm());
-  
-}
+  return false;
 }
 
+/*
+ * Function checking WiFi creds in memory 
+ * Returns: true if not empty, false if empty
+ */
+bool CheckWIFICreds(){
+  Serial.println("Checking WIFI credentials");
+  String s = EEPROM.readString(100);
+  String p = EEPROM.readString(200);
+  String d = EEPROM.readString(400);
+  String mi = EEPROM.readString(500); 
+  String ma = EEPROM.readString(600);
+  //#if DEBUG
+  Serial.print("Found credentials: ");
+  Serial.print(s);
+  Serial.print("/");
+  Serial.print(p);
+  Serial.print("/");
+  Serial.print(d);
+  Serial.print("/");
+  Serial.print(mi);
+  Serial.print("/");
+  Serial.print(ma);
+  delay(5000);
+  //#endif
+  if(s.length() > 0 && p.length() > 0 && d.length() > 0 && mi.length() > 0 && ma.length() > 0  ){
+    return true;
+  }else{
+    return false;
+  }
+}
